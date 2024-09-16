bb086a5ecd2804d3300f6c10560cf9b2fc6276d0
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:19:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+//Error
+let listing = [`anaya`, `hania`, `afza`, `fitrus`,];
+console.log(listing[4]);
+//correction
+let list = [`anaya`, `hania`, `afza`, `fitrus`, `nalain`];
+console.log(listing[4]);
+console.log(listing[0]);
+console.log(listing[1]);
+console.log(listing[2]);
+console.log(listing[3]);
---
+++ app.ts
@@ -0,0 +1,11 @@
+//Error
+let listing: string[] = [`anaya` , `hania` , `afza`  , `fitrus` , ]
+console.log(listing[4])
+
+//correction
+let list: string[] = [`anaya` , `hania` , `afza`  , `fitrus` , `nalain`]
+console.log(listing[4])
+console.log(listing[0])
+console.log(listing[1])
+console.log(listing[2])
+console.log(listing[3])
